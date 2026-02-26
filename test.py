fba2d2ca75ccc80ff9c29a21d45326ff6a7449dc
Nguyễn Khắc Quang Anh <bop0002@gmail.com>
2026-02-26 14:34:17 +0700

    Add socket connect to  unity

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import cv2
+import socket
 from cvzone.HandTrackingModule import HandDetector
 
 cam = cv2.VideoCapture(0)
@@ -9,22 +10,25 @@ cam.set(3,width)
 cam.set(4,height)
 
 detector = HandDetector(detectionCon=0.8,maxHands = 2)
-data = []
 
-
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1",5025)
+ 
 while True:
 
     success,img = cam.read()
 
     hands,img = detector.findHands(img)
-
+    data = []
+    
     if hands:
         hand = hands[0]
 
         lmList = hand['lmList']
-        print(lmList)
         for lm in lmList:
             data.extend([lm[0],height - lm[1],lm[2]])
+        print(data)
+        sock.sendto(str.encode(str(data)),serverAddressPort)
     cv2.imshow("Image",img)
 
     if(cv2.waitKey(1) & 0xFF ==ord('q')):
